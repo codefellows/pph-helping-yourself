cea924d27b27a15c4589512dd656f61081e7b8ce
JB-Tellez <jb.tellez@gmail.com>
2021-07-31 13:20:59 -0700

    matches content to pph talk

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 import os
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-import fire
 from playwright.async_api import async_playwright
 
 load_dotenv()  # take environment variables from .env.
@@ -13,51 +12,28 @@ async def main(course_key=None):
         browser = await p.chromium.launch()
         page = await browser.new_page()
 
-        courses = {
-            "java": "Code 401: Java",
-            "javascript": "Code 401: JavaScript",
-            ".net": "Code 401: ASP.NET",
-            "python": "Code 401: Python",
-            "cybersecurity": "Ops 401: Cybersecurity",
-        }
+        page_content = await get_page_content(page)
 
-        page_content = await get_page_content(page, course_key, courses)
-
-        parsed_content = parse_content(page_content, courses.get(course_key))
+        parsed_content = parse_content(page_content)
 
         output(parsed_content)
 
         await browser.close()
 
 
-async def get_page_content(page, course_key, courses):
+async def get_page_content(page):
     await page.goto(os.getenv("URL"))
-
-    await page.click("//label[text()='400: Advanced']")
-
-    if course_key and course_key not in courses:
-        print(f"Course {course_key} not found.")
-        print(f"Enter one of {[key for key in courses.keys()]}")
-        return
-
-    # all course families are selected by default
-    # so let's de-select all but desired one
-
-    for course in courses:
-        if course_key and course != course_key:
-            await page.click(f"//label[text()='{courses[course]}']")
 
     return await page.content()
 
 
-def parse_content(content, selected_course=None):
+def parse_content(content):
 
-    selected_course = selected_course or "Code Fellows 401"
     soup = BeautifulSoup(content, "html.parser")
 
     courses = soup.select(".course-calender-year-list .calendar-event")
 
-    text = f"{selected_course} Courses\n\n"
+    text = "Code Fellows Courses\n\n"
 
     for course in courses:
         text += course.h1.text + "\n"
@@ -69,13 +45,9 @@ def parse_content(content, selected_course=None):
 
 
 def output(content):
-    with open("./courses.txt", "w") as f:
+    with open("./cf-courses.txt", "w") as f:
         f.write(content)
 
 
-def run(course_key=None):
-    return asyncio.run(main(course_key))
-
-
 if __name__ == "__main__":
-    fire.Fire(run)
+    asyncio.run(main())
--- slides.md
+++ slides.md
@@ -11,14 +11,13 @@
 
 ## Links
 
-- https://scrapingant.com/blog/scrape-dynamic-website-with-python
-- https://stackoverflow.com/help/how-to-ask
-- https://stackoverflow.com/questions/1520429/is-there-a-css-selector-for-elements-containing-certain-text
-- https://devhints.io/xpath
-- https://github.com/google/python-fire
+- [Stack Overflow - How to ask a good question](https://stackoverflow.com/help/how-to-ask)
+- [Scraping Ant](https://scrapingant.com/blog/scrape-dynamic-website-with-python)
+- [CSS Select on Text](https://stackoverflow.com/questions/1520429/is-there-a-css-selector-for-elements-containing-certain-text)
+- [XPath](https://devhints.io/xpath)
+- [Python Fire](https://github.com/google/python-fire)
 
-
-### OMG
+### Another Way I Found After All Done
 
 - https://www.youtube.com/watch?v=MeBU-4Xs2RU
 - https://www.youtube.com/watch?v=B14mtXA7Tyw
